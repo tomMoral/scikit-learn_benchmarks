fdb618ba2db076620a10c46f6e0f4858aaf17382
tomMoral <thomas.moreau.2010@gmail.com>
2019-02-27 10:12:44 +0100

    ENH add benchmark n_jobs for t-SNE

--- benchmarks/manifold.py
+++ benchmarks/manifold.py
@@ -9,20 +9,20 @@ class TSNE_bench(Benchmark, Estimator):
     Benchmarks for t-SNE.
     """
 
-    param_names = ['method']
-    params = (['exact', 'barnes_hut'],)
+    param_names = ['method', 'n_jobs']
+    params = (['exact', 'barnes_hut'], Benchmark.n_jobs_vals)
 
     def setup_cache(self):
         super().setup_cache()
 
     def setup_cache_(self, params):
-        method, = params
+        method, n_jobs = params
 
         n_samples = 500 if method == 'exact' else None
 
         data = _digits_dataset(n_samples=n_samples)
 
-        estimator = TSNE(random_state=0, method=method)
+        estimator = TSNE(random_state=0, method=method, n_jobs=n_jobs, verbose=1)
 
         return data, estimator
 
